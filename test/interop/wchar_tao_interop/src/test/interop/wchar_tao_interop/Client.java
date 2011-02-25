ecf0e540124ea494523af5205fc7d2aa24051853
alexander.bykov <alexander.bykov>
2011-02-25 13:31:10 +0000

    PrismTech JAC#789: minor changes

--- test/interop/wchar_tao_interop/src/test/interop/wchar_tao_interop/Client.java
+++ test/interop/wchar_tao_interop/src/test/interop/wchar_tao_interop/Client.java
@@ -10,7 +10,7 @@ import java.io.*;
  * Created: Mon Sep  3 19:28:34 2001
  *
  * @author Nicolas Noffke
- * @version $Id: Client.java,v 1.1 2011-02-16 17:05:03 alexander.bykov Exp $
+ * @version $Id: Client.java,v 1.2 2011-02-25 13:31:10 alexander.bykov Exp $
  */
 
 public class Client
@@ -74,4 +74,3 @@ public class Client
         }
     }
 }// Client
-
